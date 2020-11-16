261d15573e4aa2a3bdf1d41a48e1ce52baba1e70
Suzanne Scott <suzanne.scott@csiro.au>
2020-11-16 23:21:19 +1100

    added seeksv bed

---
+++ scripts/write_seeksv_bed.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+import argparse
+import sys
+import csv
+import pdb
+
+def main(args):
+
+	parser = argparse.ArgumentParser(description = "convert seeksv output to bed file")
+	parser.add_argument('--seeksv-output', help = 'output from seeksv', required=True)
+	parser.add_argument('--chromlist', help = 'list of chromosomes from desired reference, one per line', required=True)
+	parser.add_argument('--output', help = 'name of output bed file', default='seeksv.bed')
+	args = parser.parse_args()
+	
+	# read chromsomes into memory
+	chroms = set()
+	with open(args.chromlist) as handle:
+		line = handle.readline().strip()
+		chroms = set(line.split(" "))
+
+	# read lines of seeksv output and write bed file
+	with open(args.seeksv_output, 'r', newline='') as seeksv, open(args.output, 'w', newline='') as bed:
+		reader = csv.DictReader(seeksv, delimiter='\t')
+		writer = csv.writer(bed, delimiter='\t')
+		
+		for row in reader:
+			if row['@left_chr'] in chroms:
+				row = (row['@left_chr'], row['left_pos'], row['left_pos'], row['left_strand'])
+				writer.writerow(row)
+			elif row['right_chr'] in chroms:
+				row = (row['right_chr'], row['right_pos'], row['right_pos'], row['right_strand'])
+				writer.writerow(row)
+	
+	
+if __name__ == "__main__":
+	main(sys.argv)
